5c457a29957c16ca0d243c399c03ba1f03d269c0
shouichi.inaba <inamondeath@yahoo.co.jp>
2022-05-02 19:32:05 +0900

    getAllTasksを追加

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -1,12 +1,16 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import utils.DBUtil;
 
 
 @WebServlet("/index")
@@ -19,8 +23,13 @@ public class IndexServlet extends HttpServlet {
     }
 
 
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                response.getWriter().append("Served at: ").append(request.getContextPath());
-        }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Task> messages = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+    }
 
 }
