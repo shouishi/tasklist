07d8fc753b0ab4a3042d55c0aceec4018d0a057e
shouichi.inaba <inamondeath@yahoo.co.jp>
2022-05-06 19:37:18 +0900

    バリデーションを追加

--- src/main/java/controllers/CreateServlet.java
+++ src/main/java/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 
@@ -42,6 +45,21 @@ public class CreateServlet extends HttpServlet {
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
 
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+
+            } else {
+
+
+
             em.persist(t);
             em.getTransaction().commit();
             request.getSession().setAttribute("flush", "登録が完了しました。");
@@ -51,4 +69,5 @@ public class CreateServlet extends HttpServlet {
         }
     }
 
+}
 }
